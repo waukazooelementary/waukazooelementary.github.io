a86284aff0a7f7d8ce982f665cc1389272d7fc48
Brittney Postma <brittney.postma@hotmail.com>
2019-08-08 08:54:16 -0400

    fixed submit form and required inputs

--- modal.js
+++ modal.js
@@ -1,51 +1,65 @@
 // Get the modal
-var modal = document.getElementById('myModal');
+var modal = document.getElementById("myModal");
 
 // Get the button that opens the modal
-const btn = document.getElementById('postForm');
+const btn = document.getElementById("postForm");
 
 // Get the <span> element that closes the modal
-const span = document.getElementsByClassName('close')[0];
+const span = document.getElementsByClassName("close")[0];
 
 // When the user clicks the button, open the modal
 
 btn.onclick = function() {
-	const selName = document.getElementById('volunteersList');
-	if (selName.selectedIndex < 1) {
-		alert('Please select your name.');
-		selName.focus();
-		return false;
-	}
-	return (modal.style.display = 'block');
+  const selName = document.getElementById("volunteersList");
+  const location = document.getElementById("class");
+  if (selName.selectedIndex < 1) {
+    alert("Please select your name.");
+    selName.focus();
+    return false;
+  } else if (location.value.length < 1) {
+    alert("Please input your location.");
+    location.focus();
+    return false;
+  }
+  return (modal.style.display = "block");
 };
 
 // When the user clicks on <span> (x), close the modal
 span.onclick = function() {
-	modal.style.display = 'none';
+  modal.style.display = "none";
 };
 
 // When the user clicks anywhere outside of the modal, close it
 window.onclick = function(event) {
-	if (event.target == modal) {
-		modal.style.display = 'none';
-	}
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
 };
 
 //Guest Modal//
-const guestModal = document.getElementById('guestModal');
-const okBtn = document.getElementById('ok');
-const guest = document.getElementById('guestName');
+const guestModal = document.getElementById("guestModal");
+const okBtn = document.getElementById("ok");
+const guest = document.getElementById("guestName");
 let hasFired = false;
 
 guest.onkeydown = function() {
-	if (!hasFired) {
-		guestModal.style.display = 'block';
-	} else {
-		guestModal.style.display = 'none';
-	}
+  if (!hasFired) {
+    guestModal.style.display = "block";
+  } else {
+    guestModal.style.display = "none";
+  }
 };
 
 okBtn.onclick = function() {
-	hasFired = true;
-	guestModal.style.display = 'none';
+  hasFired = true;
+  guestModal.style.display = "none";
+};
+
+const notes = document.getElementById("notes");
+
+const isNoteEmpty = () => {
+  if (notes.value.length < 1) {
+    notes.value = "";
+    return;
+  }
 };
